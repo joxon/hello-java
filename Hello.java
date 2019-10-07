30ff806c858238dc2f8e2af2d82ba0c1610fa461
Joxon <cijophxon@gmail.com>
2019-10-07 13:39:51 -0700

    Added some Eclipse project files

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
-  public static void main(String[] args) throws NullPointerException {
-    try {
-      System.out.println("hello");
-    } catch (NullPointerException exception) {
-      exception.printStackTrace();
-    }
+  public static void main(String[] args) {
+    System.out.println("hello");
   }
 }
