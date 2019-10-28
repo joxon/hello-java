54b4f5e804e1c0f343a45cd33cae02611367a390
Joxon <cijophxon@gmail.com>
2019-10-28 01:33:44 -0700

    Fixed ex42 7-5

--- src/uci/mswe/swe243p/ex42/7-5.sql
+++ src/uci/mswe/swe243p/ex42/7-5.sql
@@ -1,13 +1,13 @@
 select
   vendor_id,
-  max(invoice_total) as max_unpaid
+  max(invoice_total - payment_total - credit_total) as max_unpaid
 from
   invoices
-where
-  -- search those who have balance due > 0 (unpaid)
-  invoice_total - payment_total - credit_total > 0
 group by
-  vendor_id;
+  vendor_id
+having
+  -- search those who have balance due > 0 (unpaid)
+  max_unpaid > 0;
 -- sum of the largest unpaid
 select
   sum(max_unpaid)
@@ -15,11 +15,11 @@ from
   (
     select
       vendor_id,
-      max(invoice_total) as max_unpaid
+      max(invoice_total - payment_total - credit_total) as max_unpaid
     from
       invoices
-    where
-      invoice_total - payment_total - credit_total > 0
     group by
       vendor_id
+    having
+      max_unpaid > 0
   ) as max_unpaid_for_each_vendor;
