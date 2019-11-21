7c3d7abacb7d57debf437ba228fd61096bb7241e
Joxon <cijophxon@gmail.com>
2019-11-21 15:58:38 -0800

    245p ex21: Start

---
+++ src/uci/mswe/swe245p_gui/ex21_student_roster/StudentRoster.java
@@ -0,0 +1,31 @@
+package uci.mswe.swe245p_gui.ex21_student_roster;
+
+import java.util.Date;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * StudentRoster
+ */
+public class StudentRoster extends Application {
+
+  public static void main(final String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void init() throws Exception {
+    System.out.println("init(): " + new Date());
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.show();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    System.out.println("stop(): " + new Date());
+  }
+
+}
