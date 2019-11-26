1a900e81490fc05cafb4b7c562670cc0d0a63263
Joxon <cijophxon@gmail.com>
2019-11-26 02:46:50 -0800

    245 ex23: Start and use IntelliJ IDEA

---
+++ src/uci/mswe/swe245p_gui/ex23_tabs_and_stats/StudentRosterWithTabs.java
@@ -0,0 +1,19 @@
+package uci.mswe.swe245p_gui.ex23_tabs_and_stats;
+
+import javafx.stage.Stage;
+import uci.mswe.swe245p_gui.ex22_adding_a_table.StudentRosterWithTable;
+
+/**
+ * StudentRosterWithTabs
+ */
+public class StudentRosterWithTabs extends StudentRosterWithTable {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    super.start(stage);
+  }
+}
