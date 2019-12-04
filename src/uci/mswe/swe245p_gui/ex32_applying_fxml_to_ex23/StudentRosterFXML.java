0a748d1d98f8650d134dcd847860d2d77ff0a45e
Joxon <cijophxon@gmail.com>
2019-12-03 17:21:39 -0800

    245 ex32: Fixed incorrect path

--- src/uci/mswe/swe245p_gui/ex32_applying_fxml_to_ex23/StudentRosterFXML.java
+++ src/uci/mswe/swe245p_gui/ex32_applying_fxml_to_ex23/StudentRosterFXML.java
@@ -10,7 +10,7 @@ public class StudentRosterFXML extends Application {
   private static final int MIN_WINDOW_WIDTH = 1280;
   private static final int MIN_WINDOW_HEIGHT = 720;
 
-  private String fxmlName = "StudentRosterWithCSS.fxml";
+  private String fxmlName = "StudentRoster.fxml";
   private Scene scene;
 
   public static void main(String[] args) {
