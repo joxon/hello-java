1b13d8b62b38b80adc1bf81835dcb509bd383022
Junxian Chen <junxian.chen@uci.edu>
2020-05-20 19:21:10 -0700

    Start TwentySeven

---
+++ src/edu/uci/swe262p_progstyles/week8/TwentySeven.java
@@ -0,0 +1,7 @@
+package edu.uci.swe262p_progstyles.week8;
+
+/**
+ * Created by Junxian Chen on 2020-05-20.
+ */
+public class TwentySeven {
+}
---
+++ src/edu/uci/swe262p_progstyles/week8/tf-28.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# https://github.com/crista/exercises-in-programming-style/blob/master/28-lazy-rivers/tf-28.py
+
+import operator
+import string
+import sys
+
+
+def characters(filename):
+    for line in open(filename):
+        for c in line:
+            yield c
+
+
+def all_words(filename):
+    is_start_char = True
+    word = ""
+    for char in characters(filename):
+        if is_start_char:
+            word = ""
+            if char.isalnum():
+                # We found the start of a word
+                word = char.lower()
+                is_start_char = False
+            else:
+                pass
+        else:
+            if char.isalnum():
+                word += char.lower()
+            else:
+                # We found end of word, emit it
+                is_start_char = True
+                yield word
+
+
+def non_stop_words(filename):
+    stopwords = set(open('../stop_words.txt').read().split(',') + list(string.ascii_lowercase))
+    for w in all_words(filename):
+        if not w in stopwords:
+            yield w
+
+
+def count_and_sort(filename):
+    freqs, i = {}, 1
+    for w in non_stop_words(filename):
+        freqs[w] = 1 if w not in freqs else freqs[w] + 1
+        # For debugging
+        # if i % 5000 == 0:
+        #     yield sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+        i = i + 1
+    yield sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+
+
+#
+# The main function
+#
+for word_freqs in count_and_sort(sys.argv[1]):
+    print("-----------------------------")
+    for (w, c) in word_freqs[0:25]:
+        print(w, '-', c)
